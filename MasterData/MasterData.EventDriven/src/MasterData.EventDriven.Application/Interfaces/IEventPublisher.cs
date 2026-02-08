c8b16a7fbbba760838d8a4b44d3ae4006a60348d
mahdi fathian <74285697+mahdi-fathian@users.noreply.github.com>
2026-02-08 11:24:28 +0330

    Update IEventPublisher.cs

--- MasterData/MasterData.EventDriven/src/MasterData.EventDriven.Application/Interfaces/IEventPublisher.cs
+++ MasterData/MasterData.EventDriven/src/MasterData.EventDriven.Application/Interfaces/IEventPublisher.cs
@@ -2,9 +2,6 @@ using MasterData.EventDriven.Domain.Events;
 
 namespace MasterData.EventDriven.Application.Interfaces;
 
-/// <summary>
-/// رابط انتشار رویدادها - Event Publisher Interface
-/// </summary>
 public interface IEventPublisher
 {
     Task PublishEventsAsync(IEnumerable<DomainEvent> events, CancellationToken cancellationToken = default);
