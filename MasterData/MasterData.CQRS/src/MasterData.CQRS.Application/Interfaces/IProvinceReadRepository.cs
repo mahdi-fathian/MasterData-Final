37cd618049aa7b5b1dc9df45e9e867ddf0305496
mahdi fathian <74285697+mahdi-fathian@users.noreply.github.com>
2026-02-08 10:57:25 +0330

    Update IProvinceReadRepository.cs

--- MasterData/MasterData.CQRS/src/MasterData.CQRS.Application/Interfaces/IProvinceReadRepository.cs
+++ MasterData/MasterData.CQRS/src/MasterData.CQRS.Application/Interfaces/IProvinceReadRepository.cs
@@ -2,9 +2,7 @@ using MasterData.CQRS.Domain.Entities;
 
 namespace MasterData.CQRS.Application.Interfaces;
 
-/// <summary>
-/// رابط مخزن استان برای عملیات خواندنی - Province Read Repository Interface for Read Operations
-/// </summary>
+
 public interface IProvinceReadRepository
 {
     Task<Province?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
