a491d6da7842150995f54d31adfdd75151d04dcf
mahdi fathian <74285697+mahdi-fathian@users.noreply.github.com>
2026-02-08 11:14:05 +0330

    Update ProvinceCreatedDomainEvent.cs

--- MasterData/MasterData.DDD/src/MasterData.DDD.Domain/Events/ProvinceCreatedDomainEvent.cs
+++ MasterData/MasterData.DDD/src/MasterData.DDD.Domain/Events/ProvinceCreatedDomainEvent.cs
@@ -3,9 +3,6 @@ using MasterData.DDD.Domain.SeedWork;
 
 namespace MasterData.DDD.Domain.Events;
 
-/// <summary>
-/// رویداد دامنه: استان ایجاد شد - Province Created Domain Event
-/// </summary>
 public class ProvinceCreatedDomainEvent : IDomainEvent
 {
     public Province Province { get; }
