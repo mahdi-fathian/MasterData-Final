78bc899b67300956dd97f7b2cffe8465e5655d1e
mahdi fathian <74285697+mahdi-fathian@users.noreply.github.com>
2026-02-08 11:14:33 +0330

    Update ProvinceNameChangedDomainEvent.cs

--- MasterData/MasterData.DDD/src/MasterData.DDD.Domain/Events/ProvinceNameChangedDomainEvent.cs
+++ MasterData/MasterData.DDD/src/MasterData.DDD.Domain/Events/ProvinceNameChangedDomainEvent.cs
@@ -3,9 +3,7 @@ using MasterData.DDD.Domain.SeedWork;
 
 namespace MasterData.DDD.Domain.Events;
 
-/// <summary>
-/// رویداد دامنه: نام استان تغییر کرد - Province Name Changed Domain Event
-/// </summary>
+
 public class ProvinceNameChangedDomainEvent : IDomainEvent
 {
     public Province Province { get; }
