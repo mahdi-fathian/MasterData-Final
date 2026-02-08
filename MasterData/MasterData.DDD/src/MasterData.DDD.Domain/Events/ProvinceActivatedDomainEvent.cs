4b7b3f4a77ad0144279bc5a91b3a51145da611d5
mahdi fathian <74285697+mahdi-fathian@users.noreply.github.com>
2026-02-08 11:13:28 +0330

    Update ProvinceActivatedDomainEvent.cs

--- MasterData/MasterData.DDD/src/MasterData.DDD.Domain/Events/ProvinceActivatedDomainEvent.cs
+++ MasterData/MasterData.DDD/src/MasterData.DDD.Domain/Events/ProvinceActivatedDomainEvent.cs
@@ -3,9 +3,7 @@ using MasterData.DDD.Domain.SeedWork;
 
 namespace MasterData.DDD.Domain.Events;
 
-/// <summary>
-/// رویداد دامنه: استان فعال شد - Province Activated Domain Event
-/// </summary>
+
 public class ProvinceActivatedDomainEvent : IDomainEvent
 {
     public Province Province { get; }
