b0f185a48dc9be9b3c034b0950a8cb6873685210
mahdi fathian <74285697+mahdi-fathian@users.noreply.github.com>
2026-02-08 10:54:45 +0330

    Update UpdateProvinceCommand.cs

--- MasterData/MasterData.CQRS/src/MasterData.CQRS.Application/Commands/UpdateProvinceCommand.cs
+++ MasterData/MasterData.CQRS/src/MasterData.CQRS.Application/Commands/UpdateProvinceCommand.cs
@@ -2,7 +2,5 @@ using MediatR;
 
 namespace MasterData.CQRS.Application.Commands;
 
-/// <summary>
-/// دستور بروزرسانی استان - Update Province Command
-/// </summary>
+
 public record UpdateProvinceCommand(int Id, string Name) : IRequest<bool>;
