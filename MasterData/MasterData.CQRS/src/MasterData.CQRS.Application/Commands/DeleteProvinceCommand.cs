9732749915da5c55d424b5e4468e24b0637e5f65
mahdi fathian <74285697+mahdi-fathian@users.noreply.github.com>
2026-02-08 10:53:54 +0330

    Update DeleteProvinceCommand.cs

--- MasterData/MasterData.CQRS/src/MasterData.CQRS.Application/Commands/DeleteProvinceCommand.cs
+++ MasterData/MasterData.CQRS/src/MasterData.CQRS.Application/Commands/DeleteProvinceCommand.cs
@@ -2,7 +2,5 @@ using MediatR;
 
 namespace MasterData.CQRS.Application.Commands;
 
-/// <summary>
-/// دستور حذف استان - Delete Province Command
-/// </summary>
+
 public record DeleteProvinceCommand(int Id) : IRequest<bool>;
