4add0b0a17d7998afe862c8a5c81ffb73ad621ef
mahdi fathian <74285697+mahdi-fathian@users.noreply.github.com>
2026-02-08 10:55:36 +0330

    Update GetAllProvincesQueryHandler.cs

--- MasterData/MasterData.CQRS/src/MasterData.CQRS.Application/Handlers/GetAllProvincesQueryHandler.cs
+++ MasterData/MasterData.CQRS/src/MasterData.CQRS.Application/Handlers/GetAllProvincesQueryHandler.cs
@@ -5,9 +5,7 @@ using Microsoft.Extensions.Logging;
 
 namespace MasterData.CQRS.Application.Handlers;
 
-/// <summary>
-/// پردازشگر پرس‌وجوی دریافت تمام استان‌ها - Get All Provinces Query Handler
-/// </summary>
+
 public class GetAllProvincesQueryHandler : IRequestHandler<GetAllProvincesQuery, IEnumerable<ProvinceDto>>
 {
     private readonly IProvinceReadRepository _repository;
