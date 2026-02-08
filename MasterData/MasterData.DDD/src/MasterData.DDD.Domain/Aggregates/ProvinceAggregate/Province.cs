9de3a497867d08fb6ba23f185b5afbf08cef1182
mahdi fathian <74285697+mahdi-fathian@users.noreply.github.com>
2026-02-08 11:15:39 +0330

    Update Province.cs

--- MasterData/MasterData.DDD/src/MasterData.DDD.Domain/Aggregates/ProvinceAggregate/Province.cs
+++ MasterData/MasterData.DDD/src/MasterData.DDD.Domain/Aggregates/ProvinceAggregate/Province.cs
@@ -4,9 +4,6 @@ using MasterData.DDD.Domain.ValueObjects;
 
 namespace MasterData.DDD.Domain.Aggregates.ProvinceAggregate;
 
-/// <summary>
-/// ریشه مجموعه استان - Province Aggregate Root
-/// </summary>
 public class Province : Entity, IAggregateRoot
 {
     public ProvinceName Name { get; private set; }
@@ -30,18 +27,12 @@ public class Province : Entity, IAggregateRoot
         AddDomainEvent(new ProvinceCreatedDomainEvent(this));
     }
 
-    /// <summary>
-    /// ایجاد استان جدید - Create new province
-    /// </summary>
     public static Province Create(string name)
     {
         var provinceName = ProvinceName.Create(name);
         return new Province(provinceName);
     }
 
-    /// <summary>
-    /// بروزرسانی نام استان - Update province name
-    /// </summary>
     public void UpdateName(string newName)
     {
         var newProvinceName = ProvinceName.Create(newName);
@@ -57,9 +48,6 @@ public class Province : Entity, IAggregateRoot
         AddDomainEvent(new ProvinceNameChangedDomainEvent(this, oldName, newName));
     }
 
-    /// <summary>
-    /// غیرفعال کردن استان - Deactivate province
-    /// </summary>
     public void Deactivate()
     {
         if (!IsActive)
@@ -72,9 +60,6 @@ public class Province : Entity, IAggregateRoot
         AddDomainEvent(new ProvinceDeactivatedDomainEvent(this));
     }
 
-    /// <summary>
-    /// فعال کردن استان - Activate province
-    /// </summary>
     public void Activate()
     {
         if (IsActive)
