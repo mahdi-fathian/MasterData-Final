fa4e91abe4690017782144dcff29c899b7cd8bde
mahdi fathian <74285697+mahdi-fathian@users.noreply.github.com>
2026-02-08 10:57:58 +0330

    Update GetAllProvincesQuery.cs

--- MasterData/MasterData.CQRS/src/MasterData.CQRS.Application/Queries/GetAllProvincesQuery.cs
+++ MasterData/MasterData.CQRS/src/MasterData.CQRS.Application/Queries/GetAllProvincesQuery.cs
@@ -2,12 +2,8 @@ using MediatR;
 
 namespace MasterData.CQRS.Application.Queries;
 
-/// <summary>
-/// پرس‌وجوی دریافت تمام استان‌ها - Get All Provinces Query
-/// </summary>
+
 public record GetAllProvincesQuery : IRequest<IEnumerable<ProvinceDto>>;
 
-/// <summary>
-/// DTO استان - Province Data Transfer Object
-/// </summary>
+
 public record ProvinceDto(int Id, string Name, DateTime CreatedAt, DateTime? UpdatedAt);
